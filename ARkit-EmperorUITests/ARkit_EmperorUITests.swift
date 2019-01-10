131982dca1f5dfe18f1f76e6d9bb0951221d891a
Ren Matsushita <ren1725m@gmail.com>
2019-01-10 21:07:06 +0900

    Initial Commit

---
+++ ARkit-EmperorUITests/ARkit_EmperorUITests.swift
@@ -0,0 +1,34 @@
+//
+//  ARkit_EmperorUITests.swift
+//  ARkit-EmperorUITests
+//
+//  Created by Ren Matsushita on 2019/01/10.
+//  Copyright © 2019 Ren Matsushita. All rights reserved.
+//
+
+import XCTest
+
+class ARkit_EmperorUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
